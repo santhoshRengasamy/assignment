0ec4fdff8c914ea95de0a403ff378a23cceaf3cb
Santhosh Rengasamy <srengasamy@thapovan-inc.com>
2020-07-29 19:41:17 +0530

    assignment added

--- assignment1/monitsystem.sh
+++ assignment1/monitsystem.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-# Check RAM Usages
-free -h | grep -v + > /tmp/ramcache
-cat /tmp/ramcache | grep -v "Swap"
-
 # Check Disk Usages
-df -h| grep 'Filesystem\|/dev/sda*' > /tmp/diskusage
-cat /tmp/diskusage
+PART=sda1
+DISKUSE=$(df -h | grep $PART | awk '{ print $5 }' | cut -d'%' -f1)
+echo "Disk used: $DISKUSE"
+
+# CPU
+CPUUSE=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage ""}')
+echo "CPU used: $CPUUSE"
